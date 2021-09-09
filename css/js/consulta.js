c5a2b211419a1a368482a19b3b858c914335ce03
Lucimar <lucimar.neves@gmail.com>
2021-09-09 19:19:37 -0300

    barra de progresso em JavaScript

---
+++ css/js/consulta.js
@@ -0,0 +1,6 @@
+
+const progress = document.querySelector('.progress-done');
+
+progress.style.width = progress.getAttribute('data-done') + '%';
+progress.style.opacity = 1;
+
---
+++ js/consulta.js
@@ -0,0 +1,9 @@
+const progres = document.querySelector('.progress-done');
+
+
+setTimeout(() => {
+
+    progress.style.opacity = 1;
+    progress.style.width = progress.getAttribute('data-done') + '%';
+
+}, 500);
